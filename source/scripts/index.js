d1627c3097fa12422911a399ca65b3b2765dbd34
Mikhail Voronov <reatom@yandex.ru>
2024-12-08 15:49:03 +0300

    Module7-task1 (#9)
    * Добавляет прогресс по ДЗ
    
    * исправляет ошибки линтера
    
    * Еще исправляет ошибки линтера
    
    * мелкая правка в названии карточки
    
    * Добавляет сетки и декоры
    
    * Добавляет прогресс в рамках ДЗ
    
    * Добавляет секцию с котом "до/после"
    
    * Изменяет сетку в блоке с котами
    
    * Исправляет сетку в блоке с двумя черными котами
    
    * Исправляет блок с specification
    
    * исправляет паддинги и маржины на главной странице
    
    * Добавляет картинки и исправляет ошибки на главной странице
    
    * Исправляет ошибки перед защитой
    
    * Исправляет полностью блок с карточками товаров нв странице каталога на всех ширинах
    
    * Исправляет блок с картой, добавляет состояния элементов
    
    * Убирает проблему с позициоированием картинки карты
    
    * Исправляет ошибки перед PP
    
    * Правит моб версию для PP
    
    * Правки для PP моб версии
    
    * Исправления PP главная-ширина табл
    
    * Исправляет ошибки по PP на табл и деск версии главной страницы
    
    * Исправляет ошибки PP на десктопе
    
    * Исправляет ошибки PP на станице каталога
    
    * Исправляет ошибки ПП на моб каталога
    
    * Финальные правки ПП на моб каталога
    
    * Исправляет ошибки ПП табл каталога
    
    * Исправляет ошибки ПП на декстопе каталога
    
    * завершает правки ПП десктоп страницы каталога
    
    * Убирает подключение скрипта в разметке страницы каталога
    
    * исправляет ошибки линтера
    
    * Мелкие правки в блоке соц сетей
    
    * Фикс линтер
    
    * Финал фикс линтера))
    
    * Исправляет ошибки в пути к картинкам
    
    * Исправляет ошибки, выявленные наставником
    
    * Правки по зпмечаниям наставника
    
    * Правит ПП после изменений паддингов для фокуса иконок в футере
    
    * Правит ПП после изменений паддингов в иконках футера
    
    * Исправляет движение стрелки в блоке Программы при ховере.
    
    * Исправляет положение стрелки в блоке Программы на главной
    
    * Исправлят ошибки по итогам ревью наставника
    
    * Исправляет замечания наставника
    
    * Добавляет аттрибуты в разметку для автоматической проверки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,21 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-nav');
+const body = document.querySelector('body');
+const navToggle = document.querySelector('.page-header__toggle');
+
+body.classList.remove('nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+
+    navToggle.classList.remove('page-header__toggle--closed');
+    navToggle.classList.add('page-header__toggle--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+
+    navToggle.classList.add('page-header__toggle--closed');
+    navToggle.classList.remove('page-header__toggle--opened');
+  }
+});
